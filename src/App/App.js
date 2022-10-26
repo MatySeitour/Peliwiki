ab4689d92f244c00c47910a523fd76f1207aa1e9
Matias Seitour <matias.seitour01@gmail.com>
2022-10-26 00:10:15 -0300

    background mobile changes

--- src/App/App.js
+++ src/App/App.js
@@ -28,51 +28,47 @@ function App() {
   const [imageDetail, setImageDetail] = useState("");
 
   return [
-    <ThemeContextProvider key={idKeyContext}>
-      <BrowserRouter>
-        <Header
-          movieDetail={movieDetail}
-          setMovieDetail={setMovieDetail}
-        />
-        <Routes>
-          <Route exact path="/" element={<Home
+    <>
+      <div key={2} className='bg'></div>
+      <div className='content'></div>
+      <ThemeContextProvider key={idKeyContext}>
+        <BrowserRouter>
+          <Header
             movieDetail={movieDetail}
             setMovieDetail={setMovieDetail}
-            setLoading={setValue}
-            loading={value.loading}
-            imageDetail={imageDetail}
-            setImageDetail={setImageDetail}
-          />} />
-          <Route exact path='/movie/:id' element={<Details
-            imageDetail={imageDetail}
-            setImageDetail={setImageDetail}
-            movieDetail={movieDetail}
-            setMovieDetail={setMovieDetail}
-            setLoading={setValue}
-            loading={value.loading}
-          />} />
+          />
+          <Routes>
+            <Route exact path="/" element={<Home
+              movieDetail={movieDetail}
+              setMovieDetail={setMovieDetail}
+              setLoading={setValue}
+              loading={value.loading}
+              imageDetail={imageDetail}
+              setImageDetail={setImageDetail}
+            />} />
+            <Route exact path='/movie/:id' element={<Details
+              imageDetail={imageDetail}
+              setImageDetail={setImageDetail}
+              movieDetail={movieDetail}
+              setMovieDetail={setMovieDetail}
+              setLoading={setValue}
+              loading={value.loading}
+            />} />
 
-          <Route exact path='/search/:input' element={<SearchSection />} />
+            <Route exact path='/search/:input' element={<SearchSection />} />
 
-          <Route exact path='*' element={<NotFound />} />
+            <Route exact path='*' element={<NotFound />} />
 
-          <Route exact path='/series/:id' element={<SeriesDetails />} />
-          <Route exact path='/genres/:genre_name' element={<Genres />} />
-          <Route exact path='/genres/' element={<Genres />} />
+            <Route exact path='/series/:id' element={<SeriesDetails />} />
+            <Route exact path='/genres/:genre_name' element={<Genres />} />
+            <Route exact path='/genres/' element={<Genres />} />
 
-        </Routes>
+          </Routes>
 
-        <Footer />
-      </BrowserRouter>
-    </ThemeContextProvider>
-
-    // <Main
-
-    //   movieDetail={movieDetail}
-    //   setMovieDetail={setMovieDetail}
-    // />,
-
-
+          <Footer />
+        </BrowserRouter>
+      </ThemeContextProvider>
+    </>
 
   ];
 }
